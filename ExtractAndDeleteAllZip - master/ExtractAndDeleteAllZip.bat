5750f4bdee16b7705564d25359fb46ea8b8eaa20
napphuong <napphuong@gmail.com>
2017-04-04 07:10:16 -0700

    Error handling
    Handle error. Delete zip file if there is no error. Record file name if
    there is error.

--- ExtractAndDeleteAllZip - master/ExtractAndDeleteAllZip.bat
+++ ExtractAndDeleteAllZip - master/ExtractAndDeleteAllZip.bat
@@ -1,6 +1,9 @@
 FOR /F "tokens=* delims=" %%A in ('dir /b /s *.zip') do (
     7z.exe x -aoa -o"%%~dpA" "%%A" >> extract.txt
-    del "%%A"
-    echo "%%A" >> del.txt"
+    IF %ERRORLEVEL% EQU 0 (
+        del "%%A"
+    ) ELSE (
+        echo "%%A" >> error.txt
+    )
 )
 pause
